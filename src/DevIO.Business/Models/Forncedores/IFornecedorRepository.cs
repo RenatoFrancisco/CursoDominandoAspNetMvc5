dce95f17c07f19f685bcea3c8b97bd0da3e938ef
RenatoFrancisco <renatofrancisco.s@gmail.com>
2022-03-22 10:02:29 -0300

    Interfaces de acesso a dados

---
+++ src/DevIO.Business/Models/Forncedores/IFornecedorRepository.cs
@@ -0,0 +1,12 @@
+﻿using DevIO.Business.Core.Data;
+using System;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Models.Forncedores
+{
+    public interface IFornecedorRepository : IRepository<Fornecedor>
+    {
+        Task<Fornecedor> ObterFornecedoresEndereco(Guid id);
+        Task<Fornecedor> ObterFornecedoresProdutosEndereco(Guid id);
+    }
+}
