a783b65d5b95106c1287b934dcfec5e3e8d32951
RenatoFrancisco <renatofrancisco.s@gmail.com>
2022-03-25 21:05:24 -0300

    Notificacões de erros

---
+++ src/DevIO.Business/Core/Notificacoes/Notificacao.cs
@@ -0,0 +1,9 @@
+﻿namespace DevIO.Business.Core.Notificacoes
+{
+    public class Notificacao
+    {
+        public string Mensagem { get; }
+
+        public Notificacao(string mensagem) => Mensagem = mensagem;
+    }
+}
---
+++ src/DevIO.Business/Core/Notificacoes/Notificador.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DevIO.Business.Core.Notificacoes
+{
+    public class Notificador : INotificador
+    {
+        private readonly List<Notificacao> _notificacoes;
+
+        public Notificador() => _notificacoes = new List<Notificacao>();
+
+
+        public void Handle(Notificacao notificacao) => _notificacoes.Add(notificacao);
+
+        public List<Notificacao> ObterNotificacoes() => _notificacoes;
+
+        public bool TemNotificacao() => _notificacoes.Any();
+    }
+}
