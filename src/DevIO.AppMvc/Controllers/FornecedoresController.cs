e0a360b1b05b21e721a4ec0f1349cee58ee15051
RenatoFrancisco <renatofrancisco.s@gmail.com>
2022-04-08 14:22:17 -0300

    Trabalhando com ModalView pt II

--- src/DevIO.AppMvc/Controllers/FornecedoresController.cs
+++ src/DevIO.AppMvc/Controllers/FornecedoresController.cs
@@ -104,6 +104,15 @@ namespace DevIO.AppMvc.Controllers
             return RedirectToAction("Index");
         }
 
+        [Route("obter-endereco-fornecedor/{id:guid}")]
+        public async Task<ActionResult> ObterEndereco(Guid id)
+        {
+            var fornecedor = await ObterFornecedorEndereco(id);
+            if (fornecedor == null) return HttpNotFound();
+
+            return PartialView("_DetalhesEndereco", fornecedor);
+        }
+
         [Route("atualizar-endereco-fornecedor/{id:guid}")]
         [HttpGet]
         public async Task<ActionResult> AtualizarEndereco(Guid id)
@@ -113,6 +122,24 @@ namespace DevIO.AppMvc.Controllers
             if (fornecedor == null) return HttpNotFound();
 
             return PartialView("_AtualizarEndereco", new FornecedorViewModel { Endereco = fornecedor.Endereco });
+        }
+
+        [Route("atualizar-endereco-fornecedor/{id:guid}")]
+        [HttpPost]
+        public async Task<ActionResult> AtualizarEndereco(FornecedorViewModel fornecedorViewModel)
+        {
+            ModelState.Remove("Nome");
+            ModelState.Remove("Documento");
+
+            if (!ModelState.IsValid) return PartialView("_AtualizarEndereco", fornecedorViewModel);
+
+            await _fornecedorService.AtualizarEndereco(_mapper.Map<Endereco>(fornecedorViewModel.Endereco));
+
+            //TODO:
+            // E se não der certo
+
+            var url = Url.Action("ObterEndereco", "Fornecedores", new { id = fornecedorViewModel.Endereco.FornecedorId });
+            return Json(new { success = true, url });
         }
 
         private async Task<FornecedorViewModel> ObterFornecedorEndereco(Guid id) =>
